00b6766b7cf4417282d76be57bc226cdea287f04
0livierG <o.guilhaume@gmail.com>
2013-10-21 15:43:08 +0200

    Fin de la partie 2

--- src/tuto/WelcomeBundle/Controller/HomePageController.php
+++ src/tuto/WelcomeBundle/Controller/HomePageController.php
@@ -3,12 +3,30 @@
 namespace tuto\WelcomeBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class HomepageController extends Controller
 {
+    /**
+     * @param $page
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Template()
+     */
     public function indexAction($page)
     {
+        $this->get('session')->getFlashBag()->add('nom', $this->get('session')->get('user_name'));
+       // return $this->render('tutoWelcomeBundle:Homepage:index.html.twig', array('page' => $page));
+    }
 
-        return $this->render('tutoWelcomeBundle:Homepage:index.html.twig', array('page' => $page));
+    /**
+     * @param $nom
+     */
+    public function whoAmIAction($nom)
+    {
+        $session = $this->get('session');
+        $session->set('user_name',$nom);
+        return $this->redirect($this->generateUrl('homepage'));
+
     }
 }
